c89f1f40407e93180990f2ed29562b6abfe42980
Leo Schwertfuehrer <leo.schwertfuehrer@brixware.com>
2026-03-31 22:45:45 +0200

    lala

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jesicasuh-coder.github.io/portfoliooooh',
-	// base: '',
+	site: 'https://jesicasuh-coder.github.io',
+	base: '/portfoliooooh',
 });

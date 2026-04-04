cc8c036fce575aaf371e849f6137a3f1cfed0704
Daniel <d97shryu@gmail.com>
2026-04-04 13:31:25 -0700

    change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jesicasuh-coder.github.io',
-	base: '/portfoliooooh/',
+	site: 'https://jessicasuh.com'
 });
